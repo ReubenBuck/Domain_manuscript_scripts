cdaa514808af547c8b8ee777bbfe6dbe884c7525
ReubenBuckley <reuben.buckley@adelaide.edu.au>
2016-04-11 10:27:52 +0930

    Dir Sorting
    Scripts have been sorted into several dirs
    
    oldScripts - comprises a seemingly random collection of scrits that have been superseeded. I'm keeping these becasue they have come in handy in the past
    
    the rest of the dirs are self explanatory and each handle a differnet part of the analysis

---
+++ L1sizeDistribution.R
@@ -0,0 +1,19 @@
+### so what do we need?
+
+### reps
+
+### intergenic regions and intronic regions 
+
+### Domains 
+
+
+# so we want to see if the distribtuion of L1 sizes changes in different regions, 
+# we want to see the element size argument
+
+# we also want to calculate which portion of the elements are above and below certain sizes. 
+
+
+
+
+
+
